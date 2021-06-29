727b53b3d2782bbb850f41eb0487548197389b2c
angelicarm3 <angie00069@gmail.com>
2021-06-29 11:22:39 -0500

    1st commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - 
+ *
+ * Return:
+ */
+int main (void)
+{
+	int i = 97;
+	while (i <= 122)
+	{
+		putchar (i);
+		i++;
+	}
+
+	int i = 65;
+	while (i <= 90)
+	{
+		putchar(i);
+		i++;
+	}
+	return (0)
+}
