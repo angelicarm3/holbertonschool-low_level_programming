d965b77b7a42e35161871c431134f00da3670eb9
angelicarm3 <angie00069@gmail.com>
2021-07-01 17:25:31 -0500

    1st commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _islower - Checks for lowercase character.
+*
+* Return: 1 is c is lowercase or 0 otherwise
+*/
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
