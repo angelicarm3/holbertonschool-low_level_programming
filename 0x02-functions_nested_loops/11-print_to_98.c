72a48fad039a01313b0982ecd6be4d204d848b24
angelicarm3 <angie00069@gmail.com>
2021-07-02 08:28:49 -0500

    Fixed commas

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,15 +8,21 @@
 */
 void print_to_98(int n)
 {
-	while (n < 98)
+	if (n < 98)
 	{
-		printf("%d ", n);
-		n++;
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}		
 	}
-	while (n >= 98)
+	else if (n > 98)
 	{
-		printf("%d ", n);
-		n--;
+		while (n >= 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
 	}
-	printf("\n");
+	printf("%d\n", n);
 }
