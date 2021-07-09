b0d5c742e486c78942ebaf6c0e4a2675f55e2103
angelicarm3 <angie00069@gmail.com>
2021-07-09 07:53:36 -0500

    1st commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+* reset_to_98 - Updates the value of int to 98.
+*
+* @n: Pointer to the integer we wan to update to 98
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
