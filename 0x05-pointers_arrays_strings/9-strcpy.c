69e5798c394356037e95cccf0347ad9500b16c16
angelicarm3 <angie00069@gmail.com>
2021-07-14 18:59:27 -0500

    betty compliant

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
-* *_strcpy: Copies the string pointed to by src, including null byte (\0), to the buffer pointed to by dest.
+* *_strcpy: Copies the string pointed to by src, including null byte (\0),\
+* to the buffer pointed to by dest.
 *
 * @src: String to be copied.
 * @dest: Destination string.
