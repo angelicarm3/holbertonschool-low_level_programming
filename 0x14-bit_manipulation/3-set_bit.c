2404724e96025d666c9053f0bcb1982137d97c9a
angelicarm3 <angie00069@gmail.com>
2021-08-10 19:32:25 -0500

    betty compliant

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,10 +9,10 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-        if (index <= (sizeof(n)) * 8)
-        {
+	if (index <= (sizeof(n)) * 8)
+	{
 		*n = (1 << index) | *n;
 		return (1);
-        }
-        return (-1);
+	}
+	return (-1);
 }
